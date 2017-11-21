76a19d70e25cbc46722d44c4ec98633565f0ba20
Bhavik Sheth <bhaviksheth20@gmail.com>
2017-11-21 20:25:22 +0000

    add pages information to books

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'JavaScript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'JavaScript: The Good Parts' , pages: 121},
+    { title: 'Harry Potter' , pages: 239},
+    { title: 'The Dark Tower' , pages: 48},
+    { title: 'Eloquent Ruby' , pages: 384}
   ];
 }
